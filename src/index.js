564771d70750bcf6aee60616fd2cb8278a7f8381
John Kirch <john.kirch.business@gmail.com>
2018-08-28 20:33:39 -0600

    creating a react-redux template

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { promise } from "redux-promise";
+
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./App";
+import "./index.css";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
